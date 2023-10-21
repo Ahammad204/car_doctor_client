f7ea50d3ca5ed9b9516c94c34c15778294dc0315
Ahammad204 <Ahammadcode@gmail.com>
2023-10-22 01:10:50 +0600

    first commit

---
+++ src/Routes/Rotes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+
+        {
+
+            path:'/'
+
+
+        }
+
+
+      ]
+    },
+  ]);
+   
+  
+ export default router;
